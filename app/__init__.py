c955b0b13a3b105a920c9078a5a71e843b8a9fe7
Aris Setiawan Bara <baraaris2@gmail.com>
2025-12-27 10:55:59 +0800

    feat(app) : add FastAPI main function

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-import os
+from app.api.routes import router
 
 
-app = FastAPI(title="Learning RAG Demo")
+
+app = FastAPI(title="Learning RAG Demo",root_path="/api/v1")
+app.include_router(router=router)
+
+
+@app.get("/healthy")
+async def healthy():
+    return {"message": "healthy oke"}
