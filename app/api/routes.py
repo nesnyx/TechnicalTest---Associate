ac0d154d6ab8a3f1a94f051532cdf2b1f209dcc0
Aris Setiawan Bara <baraaris2@gmail.com>
2025-12-27 10:54:35 +0800

    feat(api) : add routes api router

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter
-import time
+from fastapi import APIRouter,HTTPException
+import time,os
+from dotenv import load_dotenv
 from app.models.schemas import QuestionRequest, DocumentRequest
+from app.services.embeddings import EmbeddingService
+from app.storage.qdrant_store import DocumentStore
+from app.services.rag_workflow import RagWorkflow
 
-routes = APIRouter()
+load_dotenv()
+QDRANT_URL = os.getenv("QUANDRANT_URL")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")    
 
+router = APIRouter()
+store = DocumentStore(qdrant_url=QDRANT_URL, collection_name=COLLECTION_NAME)
+embedder = EmbeddingService()
+rag_workflow = RagWorkflow(embbeder=embedder, store=store)
 
-
-@routes.post("/ask")
+@router.post("/ask")
 def ask_question(req: QuestionRequest):
     start = time.time()
     try:
-        result = chain.invoke({"question": req.question})
+        result = rag_workflow.chain.invoke({"question": req.question})
         return {
             "question": req.question,
             "answer": result["answer"],
@@ -19,3 +28,23 @@ def ask_question(req: QuestionRequest):
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+@router.post("/add")
+def add_document(req: DocumentRequest):
+    try:
+        emb = embedder.embed(req.text)
+        store.add(req.text, emb)
+        doc_id = len(store.docs_memory)
+        return {"id": doc_id, "status": "added"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/status")
+def status():
+    return {
+        "qdrant_ready": store.using_qdrant,
+        "in_memory_docs_count": len(store.docs_memory),
+        "graph_ready": rag_workflow.chain is not None
+    }
